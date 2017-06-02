d824a970ae0d0d9b19d0ccdc11a725b8938ab1a1
Yunsik Park <space214@gmail.com>
2017-06-02 12:43:15 +0900

    add namespace to polls/urls.py, use url tag in template

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # get_list_or404를 사용한 경우
+    latest_question_list = get_list_or_404(
+        Question.objects.order_by('-pub_date')[:5]
+    )
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list,
     }
@@ -16,15 +20,21 @@ def detail(request, question_id):
     # question_id 가 pk인 Question 객체를 가저와
     # context라는 이름을 가진 dict에 'question'이라는 키 값으로 위 변수를 할당
     # 이 후 'polls/detail.html'과 context를 랜더한 결과를 리턴
-    try:
-        question =  Question.objects.get(pk = question_id)
-    except Question.DoesNotExist as e:
-        raise Http404('Question does not exist')
+
+    # try:
+    #     question =  Question.objects.get(pk = question_id)
+    # except Question.DoesNotExist as e:
+    #     raise Http404('Question does not exist')
+
+    # question.choice_set.
+    # Choice.objects.filter(question=question)
+
+
+    question = get_object_or_404(Question, pk=question_id)
     context = {
-        'question':question,
+        'question': question,
     }
     return render(request, 'polls/detail.html', context)
-
 
 
 def results(request, question_id):
